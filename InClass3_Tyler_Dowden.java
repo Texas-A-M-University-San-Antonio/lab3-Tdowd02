8a4e09ff35dbb336c0b962c50f6c62878a5b826e
Tdowd02 <tyler.dowden05@gmail.com>
2025-09-17 00:32:43 +0000

    Finished in class lab

---
+++ InClass3_Tyler_Dowden.java
@@ -0,0 +1,43 @@
+/**
+ * Name: Tyler Dowden
+ * Date: 9/16/2025
+* Purpose: InClass 3 - 
+* [Write a description about this InClass 3 program]
+ */
+
+        import java.util.Scanner; //Imports the scanner from library
+
+// Don't forget to import and declare a Scanner for user input.
+
+public class InClass3_Tyler_Dowden
+{
+    public static void main(String[] args) 
+    {       
+            Scanner input = new Scanner(System.in); // Make scanner
+
+        // In-Class 3 Example:
+        // Step 1: Prompt the user to enter the length of the side of a cube.
+
+            System.out.println("Length of sides: "); // Prompts user to input length of the cube sides
+
+        // Step 2: Read the user's input and store it in a variable.
+
+            double length = input.nextDouble(); // Stores user input in length
+
+        // Step 3: Use an appropriate method to determine the volume of the cube.
+
+            double volume = Math.pow(length, 3); // Calculates the volume
+            
+        // Step 4: Use an appropriate method to determine the surface area of the cube.
+
+            double SA = Math.pow(length, 2); // Calculates the surface area
+            SA = 6*SA;
+
+        // HINT: Steps 3 and 4 will use the Math.pow(a, b) method
+        // Step 5: Print the volume and surface area in clear sentences.
+
+        System.out.println("The volume of the cube is "+ volume); // prints the volume
+        System.out.println("The surface area of the cube is "+ SA); // prints the surface area
+
+    }
+}
